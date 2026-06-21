41c3225e4de043a4c869bdc0508020aa24c6c9d5
ahmedzahran <ahmedzahran1994@gmail.com>
2026-06-21 03:16:39 +0300

    CQRS pattern

--- order-service/order-dataacess/src/main/java/com/food/ordering/system/order/service/dataacess/customer/entity/CustomerEntity.java
+++ order-service/order-dataacess/src/main/java/com/food/ordering/system/order/service/dataacess/customer/entity/CustomerEntity.java
@@ -3,21 +3,22 @@ package com.food.ordering.system.order.service.dataacess.customer.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "order_customer_m_view",schema = "customer")
+@Table(name = "customers")
 @Entity
 public class CustomerEntity {
 
     @Id
     private UUID id;
+    private String username;
+    private String firstName;
+    private String lastName;
 }
